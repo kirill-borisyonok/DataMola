d99a17af068366e2f1cfb4a8313712ebdcb8f95a
Kirill Borisyonok <boravdv@mail.ru>
2022-03-31 11:03:17 +0300

    fixed Tweet.js and main.html

--- UI/Tweet.js
+++ UI/Tweet.js
@@ -352,7 +352,7 @@ class TweetFeed {
         let filterTweetsArr = this.tweets.slice();
 
         // сработает в случае наличия filterConfig
-        if (filterConfig) {
+        if (Object.keys(filterConfig).length !== 0) {
             let count = 0;
             // фильтрация по имени автора
             if ('author' in filterConfig) {
@@ -488,3 +488,265 @@ tweetFeed.addAll(tweets);
 // console.log(tweetFeed.get('20'));
 
 // console.log(tweetFeed);
+
+class HeaderView {
+    constructor(containerId) {
+        this.containerId = document.getElementById(containerId);
+    }
+
+    display(user) {
+        this.containerId.textContent = user;
+    }
+}
+
+// let user = new HeaderView('userName');
+// user.display('Петр Петров');
+
+class TweetFeedView {
+    constructor(containerId) {
+        this.containerId = document.getElementById(containerId);
+    }
+
+    display(tweetsArr) {
+        const date = (date) => {
+            const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            let dateGood = `${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}`;
+            return dateGood;
+        };
+
+        const time = (time) => {
+            let hoursGood = `${time.getHours()}`;
+            if (hoursGood.length === 1) {
+                hoursGood = `0${hoursGood}`;
+            }
+
+            let minutesGood = `${time.getMinutes()}`;
+            if (minutesGood.length === 1) {
+                minutesGood = `0${minutesGood}`;
+            }
+
+            let timeGood = `${hoursGood}:${minutesGood}`;
+            return timeGood;
+        };
+
+        const textGood = (text) => {
+            text = text.split(' ')
+                .map(item => {
+                    if (item[0] === '#') return `<span class="tweets__tags">${item}</span>`;
+                    return item;
+                })
+                .join(' ');
+            return text;
+        };
+
+        const tweetsItems = tweetsArr.map(item => `<div id="${item.id}" class="tweets__item">
+                            <div class="tweets__item-inner">  
+                                <img class="tweets__item-img" src="images/my_photo.png" alt="user photo">
+                                
+                                <div class="tweets__item-content">
+                                    <div class="tweets__item-title">
+                                        <a class="tweets__item-author" href="#">${item.author}</a>
+                                        <time pubdate class="tweets__item-date"><span class="dot">&#149</span> ${date(item.createdAt)} <b>${time(item.createdAt)}</b></time>
+                                    </div>
+                                    <div class="tweets__text">${textGood(item.text)}</div>
+                                </div> 
+                            </div>
+                            <div class="tweets__item-footer">
+                                <div class="comments">
+                                    <svg class="comments__img">
+                                        <use xlink:href="#message"></use>
+                                    </svg>
+                                    <div class="comments__text">${item.comments.length}</div>
+                                </div>
+                                <div class="tweets__item-footer-button">
+                                    <button class="btn--sml btn--white" type="button">Delete</button>
+                                    <button class="btn btn--sml" type="button">Edit</button>
+                                </div>
+                            </div>
+                        </div>`).join('\n');
+
+        this.containerId.innerHTML = `${tweetsItems}`;
+    }
+}
+
+// let tweetsFeed = new TweetFeedView('tweetsTape');
+// tweetsFeed.display(tweets);
+
+class TweetView {
+    constructor(containerId) {
+        this.containerId = document.getElementById(containerId);
+    }
+
+    display(tweet) {
+        let date = (date) => {
+            const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            let dateGood = `${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}`;
+            return dateGood;
+        };
+
+        let time = (time) => {
+            let hoursGood = `${time.getHours()}`;
+            if (hoursGood.length === 1) {
+                hoursGood = `0${hoursGood}`;
+            }
+
+            let minutesGood = `${time.getMinutes()}`;
+            if (minutesGood.length === 1) {
+                minutesGood = `0${minutesGood}`;
+            }
+
+            let timeGood = `${hoursGood}:${minutesGood}`;
+            return timeGood;
+        };
+
+        let textGood = (text) => {
+            text = text.split(' ')
+                .map(item => {
+                    if (item[0] === '#') return `<span class="tweets__tags">${item}</span>`;
+                    return item;
+                })
+                .join(' ');
+            return text;
+        };
+
+        const tweetItem = `<div id="${tweet.id}" class="tweets__item tweets__item--page">
+                <div class="tweets__item-inner">  
+                <img class="tweets__item-img" src="images/my_photo.png" alt="user photo">
+            
+                <div class="tweets__tape-item-content">
+                    <div class="tweets__item-title">
+                        <a class="tweets__item-author" href="#">Kirill Borisyonok</a>
+                        <time pubdate class="tweets__item-date"><span class="dot">&#149</span> ${date(tweet.createdAt)} <b>${time(tweet.createdAt)}</b></time>
+                    </div>
+                    <div class="tweets__text">${textGood(tweet.text)}</div>
+                </div> 
+                </div>
+                <div class="tweets__item-footer">
+                    <div class="comments">
+                        <svg class="comments__img">
+                            <use xlink:href="#message"></use>
+                        </svg>
+                        <div class="comments__text">${tweet.comments.length}</div>
+                    </div>
+                    <div class="tweets__item-footer-button">
+                        <button class="btn--sml btn--white" type="button">Delete</button>
+                        <button class="btn btn--sml" type="button">Edit</button>
+                    </div>
+                </div>
+            </div>`;
+
+        const comments = tweet.comments.map(comment => `<div id="${comment.id}" class="comment__item">
+                <div class="comment__item-inner">  
+                    <img class="comment__item-img" src="images/my_photo.png" alt="user photo">
+                        
+                    <div class="comment__item-content">
+                        <div class="comment__item-title">
+                            <a class="comment__item-author" href="#">${comment.author}</a>
+                            <time pubdate class="comment__item-date">${date(comment.createdAt)} <b>${time(comment.createdAt)}</b></time>
+                        </div>
+                        <div class="comment__text">${textGood(comment.text)}))</div>
+                    </div> 
+                </div>
+            </div>`);
+
+        this.containerId.innerHTML = `${tweetItem}\n
+                <div class="comment-title">Comments</div>\n
+                ${comments}\n
+                <div class="comment__set">
+                <div class="comment__set-inner">
+                    <img class="comment__set-photo" src="images/my_photo.png" alt="user photo">
+                    <form class="comment__set-form" action="/" method="post">
+                        <textarea class="comment__set-form-input" type="text" maxlength="280" placeholder="Enter your comment"></textarea>
+                        <div class="comment__set-form-button">
+                            <button type="submit" class="btn--submit" href="#">Add</button>
+                        </div> 
+                    </form>
+                </div> <!-- /.comment__set -->
+                
+            </div> `;
+    }
+}
+
+// let tweet = new TweetView('tweetsContainer');
+// tweet.display(tweets[19]);
+
+class FilterView {
+    constructor(containerId) {
+        this.containerId = document.getElementById(containerId);
+    }
+
+    display(tweetsArr) {
+        let authorArr = tweetsArr.map(item => item.author);
+        authorArr = Array.from(new Set(authorArr));
+        console.log(authorArr);
+        this.containerId.insertAdjacentHTML('afterend', `<datalist id="dataLIst">
+        ${authorArr.map(item => `<option value="${item}"></option>`).join('\n')}</datalist>`);
+    }
+}
+
+// let filterView = new FilterView('filterAuthor');
+// console.log(filterView.display(tweets));
+
+function setCurrentUser(user) {
+    let tweetFeeds = new TweetFeed();
+    tweetFeeds.user = user;
+    let userHead = new HeaderView('userName');
+    userHead.display(user);
+}
+
+// setCurrentUser('Kirill');
+
+function addTweet(text) {
+    let tweetArr = new TweetFeed();
+    tweetArr.addAll(tweets);
+    tweetArr.user = 'Kirill';
+    tweetArr.add(text);
+    let tweetsFeed = new TweetFeedView('tweetsTape');
+    tweetsFeed.display(tweetArr.tweets);
+}
+
+// addTweet('123');
+
+function editTweet(id, text) {
+    let tweetArr = new TweetFeed();
+    tweetArr.addAll(tweets);
+    tweetArr.user = 'Петров Петр';
+    tweetArr.edit(id, text);
+    let tweetsFeed = new TweetFeedView('tweetsTape');
+    tweetsFeed.display(tweetArr.tweets);
+}
+
+// editTweet('2', 'text');
+
+function removeTweet(id) {
+    let tweetArr = new TweetFeed();
+    tweetArr.addAll(tweets);
+    tweetArr.user = 'Петров Петр';
+    tweetArr.remove(id);
+    let tweetsFeed = new TweetFeedView('tweetsTape');
+    tweetsFeed.display(tweetArr.tweets);
+}
+
+// removeTweet('4')
+
+function getFeed(skip = 0, top = 10, filterConfig = {}) {
+    let tweetArr = new TweetFeed();
+    tweetArr.addAll(tweets);
+    tweetArr.user = 'Петров Петр';
+    let tweetFilter = tweetArr.getPage(skip, top, filterConfig);
+    let tweetsFeed = new TweetFeedView('tweetsTape');
+    tweetsFeed.display(tweetFilter);
+}
+
+// getFeed(0, 5);
+
+function showTweet(id) {
+    let tweetArr = new TweetFeed();
+    tweetArr.addAll(tweets);
+    tweetArr.user = 'Петров Петр';
+    let tweetGet = tweetArr.get(id);
+    let tweet = new TweetView('tweetsContainer');
+    tweet.display(tweetGet);
+}
+
+// showTweet('2');
